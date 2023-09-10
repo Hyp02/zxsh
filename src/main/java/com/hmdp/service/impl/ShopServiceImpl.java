5b0fa0e0ba84192bb3a656af6e9ebd1d334a7848
hyp <2323720233@qq.com>
2023-09-10 13:45:00 +0800

    店铺缓存更新，解决数据缓存一致问题

--- src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/ShopServiceImpl.java
@@ -11,12 +11,14 @@ import com.hmdp.utils.RedisConstants;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
+import java.util.concurrent.TimeUnit;
 
 /**
  * <p>
- *  服务实现类
+ * 服务实现类
  * </p>
  *
  * @author 虎哥
@@ -31,6 +33,7 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
 
     /**
      * 查询店铺信息并使用redis缓存
+     *
      * @param id
      * @return
      */
@@ -39,7 +42,7 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
         // 查询redis【这里的数据类型可选择Hash和String(这次选择String)】
         String shopJson = stringRedisTemplate.opsForValue().get(RedisConstants.CACHE_SHOP_KEY + id);
         // 命中 返回
-        if (StringUtils.isNotBlank(shopJson)){
+        if (StringUtils.isNotBlank(shopJson)) {
             // 转换为对象后返回
             Shop shop = JSONUtil.toBean(shopJson, Shop.class);
             return Result.ok(shop);
@@ -47,12 +50,38 @@ public class ShopServiceImpl extends ServiceImpl<ShopMapper, Shop> implements IS
         // 未命中 在数据库中查
         // 数据库中不存在 返回错误信息【getById是接口中的方法】
         Shop shop = getById(id);
-        if (shop == null){
+        if (shop == null) {
             return Result.fail("对不起，该店铺已消失");
         }
         String toJsonShop = JSONUtil.toJsonStr(shop);
         // 数据库中存在 先保存在redis中再返回
-        stringRedisTemplate.opsForValue().set(RedisConstants.CACHE_SHOP_KEY+id,toJsonShop);
+        // 设置店铺缓存失效时间
+        stringRedisTemplate.opsForValue().set(RedisConstants.CACHE_SHOP_KEY + id, toJsonShop,
+                RedisConstants.CACHE_SHOP_TTL, TimeUnit.MINUTES);
         return Result.ok(shop);
+    }
+
+    /**
+     * 更新店铺
+     * 确保缓存数据一致
+     *
+     * @param shop 店铺对象
+     * @return
+     */
+    @Override
+    @Transactional //添加事务
+    public Result updateShopById(Shop shop) {
+        Long id = shop.getId();
+        if (id == null) {
+            return Result.fail("餐厅不存在");
+        }
+        // 修改数据库
+        boolean b = updateById(shop);
+        if (!b){
+            return Result.fail("更新店铺信息失败");
+        }
+        // 删除缓存
+        stringRedisTemplate.delete(RedisConstants.CACHE_SHOP_KEY + id);
+        return Result.ok();
     }
 }
