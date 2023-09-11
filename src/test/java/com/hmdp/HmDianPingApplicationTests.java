82f3750ecb0345ebcce86aef4caf5eba69c653cb
hyp <2323720233@qq.com>
2023-09-11 16:53:46 +0800

    热点店铺存放到缓存

--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -1,9 +1,28 @@
 package com.hmdp;
 
+import com.hmdp.service.impl.ShopServiceImpl;
+import com.hmdp.utils.HotShop;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.annotation.Resource;
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootTest
 class HmDianPingApplicationTests {
+    @Resource
+    private ShopServiceImpl shopService;
 
+    /**
+     * 写入一个店铺数据，并且设置逻辑过期时间
+     */
+    @Test
+    public void testAddHotShop(){
+        List<Long> longs = Arrays.asList(HotShop.hotShopId);
+        for (Long id : longs) {
+            shopService.addExpiredTimeByShop(id, 10L);
+        }
+    }
 
 }
