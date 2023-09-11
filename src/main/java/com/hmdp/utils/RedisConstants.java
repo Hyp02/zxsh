06c032c9b348c0c1d1fce970c403d0d7d29fc790
hyp <2323720233@qq.com>
2023-09-11 15:34:21 +0800

    全局常量类 Redis中Key和TTL的定义

--- src/main/java/com/hmdp/utils/RedisConstants.java
+++ src/main/java/com/hmdp/utils/RedisConstants.java
@@ -6,14 +6,14 @@ public class RedisConstants {
     public static final String LOGIN_USER_KEY = "login:token:";
     public static final Long LOGIN_USER_TTL = 30L;
     public static final String CATCH_SHOP_LIST = "list";
+    public static final String LOCK_SHOP_KEY = "lock:shop";
+    public static final Long LOCK_SHOP_TTL = 10L;
+
 
     public static final Long CACHE_NULL_TTL = 2L;
 
     public static final Long CACHE_SHOP_TTL = 30L;
     public static final String CACHE_SHOP_KEY = "cache:shop:";
-
-    public static final String LOCK_SHOP_KEY = "lock:shop:";
-    public static final Long LOCK_SHOP_TTL = 10L;
 
     public static final String SECKILL_STOCK_KEY = "seckill:stock:";
     public static final String BLOG_LIKED_KEY = "blog:liked:";
