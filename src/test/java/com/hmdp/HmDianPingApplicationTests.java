591853de7be68eba914df37976a8eb2767986a78
hyp <2323720233@qq.com>
2023-09-12 19:29:21 +0800

    更新测试类，使用封装的缓存工具类进行测试。

--- src/test/java/com/hmdp/HmDianPingApplicationTests.java
+++ src/test/java/com/hmdp/HmDianPingApplicationTests.java
@@ -1,16 +1,22 @@
 package com.hmdp;
 
+import com.hmdp.entity.Shop;
 import com.hmdp.service.impl.ShopServiceImpl;
+import com.hmdp.utils.CacheClient;
 import com.hmdp.utils.HotShop;
+import com.hmdp.utils.RedisConstants;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import javax.annotation.Resource;
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @SpringBootTest
 class HmDianPingApplicationTests {
+    @Resource
+    private CacheClient cacheClient;
     @Resource
     private ShopServiceImpl shopService;
 
@@ -21,7 +27,8 @@ class HmDianPingApplicationTests {
     public void testAddHotShop(){
         List<Long> longs = Arrays.asList(HotShop.hotShopId);
         for (Long id : longs) {
-            shopService.addExpiredTimeByShop(id, 10L);
+            Shop shop = shopService.getById(id);
+            cacheClient.setWithLogicExpired(RedisConstants.CACHE_SHOP_KEY+id,shop,20L, TimeUnit.SECONDS);
         }
     }
 
