25f63f2c79eeaac33eb4fe718dd14d57e56dcf58
hyp <2323720233@qq.com>
2023-09-19 19:39:59 +0800

    使用lua脚本解决多线程情况下锁被误删问题

--- src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -1,9 +1,14 @@
 package com.hmdp.utils;
 
 import cn.hutool.core.lang.UUID;
+import org.apache.ibatis.javassist.ClassPath;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
+import org.springframework.data.redis.core.script.RedisScript;
 
 import javax.annotation.Resource;
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -15,6 +20,14 @@ public class SimpleRedisLock implements ILock {
 
     private static final String ID_PREFIX = UUID.randomUUID().toString(true) + "-";
     private static final String KEY_PREFIX = "lock:";
+    private static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
+
+    static {
+        UNLOCK_SCRIPT = new DefaultRedisScript<>();
+        UNLOCK_SCRIPT.setLocation(new ClassPathResource("unlock.lua"));
+        UNLOCK_SCRIPT.setResultType(Long.class);
+    }
+
     @Resource
     private StringRedisTemplate stringRedisTemplate;
     private String name;
@@ -46,19 +59,36 @@ public class SimpleRedisLock implements ILock {
 
     /**
      * 释放锁
-     * 优化，防止删除不属于自己的锁
+     * 优化，防止删除不属于自己的锁 使用lua脚本
+     * 1.第一个线程进来 阻塞 锁超时释放
+     * 2.第二个进来 因为锁超时释放拿到锁
+     * 3.这时，第一个线程阻塞完毕，因为在2中 第二个线程进来将ARGV设置成了第二个线程的id，
+     * 4 这时候1线程阻塞完毕，进行删锁时，发现key对应的值不同，所以不会发生误删
      */
     @Override
     public void delLock() {
-        // 获取uuid+线程id
-        String threadId = ID_PREFIX + Thread.currentThread().getId();
-        // 获取redis中的id
-        String id = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
-        // 判断是否是自己的锁
-        // 比较当前线程id和redis中的值是否相同
-        if (threadId.equals(id)) {
-            stringRedisTemplate.delete(KEY_PREFIX + name);
-        }
-
+        //
+        stringRedisTemplate.execute(UNLOCK_SCRIPT,
+                Collections.singletonList(KEY_PREFIX + name),
+                ID_PREFIX + Thread.currentThread().getId()
+        );
     }
+    //**
+    // * 释放锁
+    // * 优化，防止删除不属于自己的锁
+    // */
+
+    // @Override
+    // public void delLock() {
+    //    // 获取uuid+线程id
+    //    String threadId = ID_PREFIX + Thread.currentThread().getId();
+    //    // 获取redis中的id
+    //    String id = stringRedisTemplate.opsForValue().get(KEY_PREFIX + name);
+    //    // 判断是否是自己的锁
+    //    // 比较当前线程id和redis中的值是否相同
+    //    if (threadId.equals(id)) {
+    //        stringRedisTemplate.delete(KEY_PREFIX + name);
+    //    }
+    //
+    //}
 }
