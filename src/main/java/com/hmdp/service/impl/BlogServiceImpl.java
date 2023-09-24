86bccb3b17a53d23aefb4a8d3749f4e35f3efdcd
hyp <2323720233@qq.com>
2023-09-24 14:48:34 +0800

    添加发布信息完整性校验

--- src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/BlogServiceImpl.java
@@ -1,5 +1,8 @@
 package com.hmdp.service.impl;
 
+import java.time.LocalDateTime;
+
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Blog;
 import com.hmdp.mapper.BlogMapper;
 import com.hmdp.service.IBlogService;
@@ -8,7 +11,7 @@ import org.springframework.stereotype.Service;
 
 /**
  * <p>
- *  服务实现类
+ * 服务实现类
  * </p>
  *
  * @author 虎哥
@@ -17,4 +20,22 @@ import org.springframework.stereotype.Service;
 @Service
 public class BlogServiceImpl extends ServiceImpl<BlogMapper, Blog> implements IBlogService {
 
+    @Override
+    public Result saveBlog(Blog blog) {
+        // 校验
+        Long shopId = blog.getShopId();
+        String title = blog.getTitle();
+        String content = blog.getContent();
+        if (shopId == null) {
+            return Result.fail("你还未关联店铺哦");
+        }
+        if (title == null) {
+            return Result.fail("请填写标题");
+        }
+        if (content == null) {
+            return Result.fail("请填写评价");
+        }
+        this.save(blog);
+        return Result.ok(blog.getId());
+    }
 }
