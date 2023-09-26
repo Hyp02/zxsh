35d78126bb9b46fe18b9ca39c24aabcd4b803e3c
hyp <2323720233@qq.com>
2023-09-26 16:48:17 +0800

    关注业务接口

--- src/main/java/com/hmdp/controller/FollowController.java
+++ src/main/java/com/hmdp/controller/FollowController.java
@@ -1,9 +1,11 @@
 package com.hmdp.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.hmdp.dto.Result;
+import com.hmdp.service.impl.FollowServiceImpl;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import javax.annotation.Resource;
 
 /**
  * <p>
@@ -16,5 +18,39 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/follow")
 public class FollowController {
+    @Resource
+    private FollowServiceImpl followService;
+
+    /**
+     * 查询是否关注
+     * @param followUserId 博主id
+     * @return
+     */
+    @GetMapping("/or/not/{followUserId}")
+    public Result isFollow(@PathVariable("followUserId") Long followUserId) {
+        return followService.isFollow(followUserId);
+    }
+
+    /**
+     * 关注博主
+     * @param followUserId
+     * @param is
+     * @return
+     */
+    @PutMapping("/{followUserId}/{isFollow}")
+    public Result follow(@PathVariable("followUserId") Long followUserId,
+                           @PathVariable("isFollow") boolean is) {
+        return followService.follow(followUserId,is);
+    }
+
+    /**
+     * 共同关注
+     * @return
+     */
+    @GetMapping("/common/{id}")
+    public Result commonUser(@PathVariable("id")Long userId){
+        return followService.commonFollow(userId);
+
+    }
 
 }
