07326796a93883329ae522a46bc0683cdfed4af8
hyp <2323720233@qq.com>
2023-10-01 13:24:06 +0800

    新增用户签到和统计连续签到天数接口

--- src/main/java/com/hmdp/controller/UserController.java
+++ src/main/java/com/hmdp/controller/UserController.java
@@ -1,19 +1,29 @@
 package com.hmdp.controller;
 
 
+import cn.hutool.core.bean.BeanUtil;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.hmdp.dto.LoginFormDTO;
 import com.hmdp.dto.Result;
 import com.hmdp.dto.UserDTO;
+import com.hmdp.entity.Blog;
 import com.hmdp.entity.User;
 import com.hmdp.entity.UserInfo;
 import com.hmdp.service.IUserInfoService;
 import com.hmdp.service.IUserService;
+import com.hmdp.service.impl.BlogServiceImpl;
+import com.hmdp.utils.SystemConstants;
 import com.hmdp.utils.UserHolder;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import java.util.List;
+
+import static com.hmdp.utils.RedisConstants.LOGIN_USER_KEY;
 
 /**
  * <p>
@@ -33,6 +43,10 @@ public class UserController {
 
     @Resource
     private IUserInfoService userInfoService;
+    @Resource
+    private BlogServiceImpl blogService;
+    @Resource
+    private StringRedisTemplate stringRedisTemplate;
 
 
 
@@ -64,9 +78,16 @@ public class UserController {
      * @return 无
      */
     @PostMapping("/logout")
-    public Result logout() {
+    public Result logout(HttpServletRequest request) {
         // TODO 实现登出功能
-        return Result.fail("功能未完成");
+        String token = request.getHeader("authorization");
+        if ("".equals(token) || token == null ) {
+            return Result.fail("退出失败");
+        }
+        UserHolder.removeUser();
+        String userToken = LOGIN_USER_KEY + token;
+        stringRedisTemplate.delete(userToken);
+        return Result.ok("退出成功，请刷新页面");
     }
 
     @GetMapping("/me")
@@ -89,5 +110,37 @@ public class UserController {
         info.setUpdateTime(null);
         // 返回
         return Result.ok(info);
+    }
+
+    /**
+     * 显示博主资料
+     * @param userId
+     * @return
+     */
+    @GetMapping("/{id}")
+    public Result blog(@PathVariable("id") Long userId){
+        // 根据返回的博主的id查询
+        User user = userService.getById(userId);
+        if (user == null) {
+            return Result.ok();
+        }
+        // 返回安全信息
+        UserDTO userDTO = BeanUtil.copyProperties(user, UserDTO.class);
+        return Result.ok(userDTO);
+
+    }
+    /**
+     * 用户签到
+     */
+    @PostMapping("/sign")
+    public Result sign(){
+        return userService.sign();
+    }
+    /**
+     * 统计用户连续签到
+     */
+    @GetMapping("/sign/count")
+    public Result signCount(){
+        return userService.signCount();
     }
 }
